3914fb4fe8b9f3710894e490f9ce35f45d777c67
Stas <chstas2@gmail.com>
2016-08-18 21:46:15 +0300

    add comments

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react'
+import Comments from './Comments'
 
 export default class Article extends Component {
-/*
 
-    constructor() {
-        super()
-        this.state = {
-            isOpen: false
-        }
-    }
-
-*/
     state = {
         isOpen: false
     }
 
     render() {
         const { article } = this.props
-        const body = this.state.isOpen ? <section>{article.text}</section> : null
+        const body = this.state.isOpen ? <section>{article.text}<Comments article = { article }/></section> : null
         return (
             <div>
                 <h3 onClick = {this.handleClick}>{article.title}</h3>
---
+++ src/components/Comments.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Comments extends Component {
+    state = {
+        isOpen: false
+    }
+
+    handleClick = (ev) => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        const { article } = this.props;
+        const commentTitle = this.state.isOpen ? <p>hide comments</p> : <p>show comments</p>
+        const commentBody = this.state.isOpen ? article.comments.map(commentsObject=> <li key = {commentsObject.id}><strong>{commentsObject.user}</strong>
+        <p>{commentsObject.text}</p>
+        </li>) : null
+        return (
+            <ul>
+                <p onClick={this.handleClick}>{commentTitle}</p>
+                {commentBody}
+            </ul>
+        )
+    }
+}
