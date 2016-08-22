f942caa331a94fbce1b98ba4344097b62f954050
Roman <kaboom.ua@gmail.com>
2016-08-22 23:08:33 +0300

    Update toggleOpenArticle.js

--- src/decorators/toggleOpenArticle.js
+++ src/decorators/toggleOpenArticle.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+//Декораторы создают для переиспользования кода. Их следует делать максимеально универсальными, 
+//не привязывайся к названиям сущностей типа Article. Лучше называй toggleOpenItem, например
 export default (Component) => {
     return class toggleOpenArticleComponent extends React.Component {
         state = {
