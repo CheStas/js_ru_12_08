32ad994313ccbf33693ce079aed1bda62b4229b2
Stas <chstas2@gmail.com>
2016-08-25 08:50:02 +0300

    add day-picker

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,14 +4,33 @@ import Select from 'react-select'
 import 'react-select/dist/react-select.css'
 import JqueryComponent from './JqueryComponent'
 import { findDOMNode } from 'react-dom'
+import DayPicker, { DateUtils } from "react-day-picker";
+import 'react-day-picker/lib/style.css';
+import moment from 'moment';
 
 class Container extends Component {
-    static propTypes = {
-
-    };
+    constructor(props) {
+        super(props);
+        this.handleDayClick = this.handleDayClick.bind(this)
+        this.handleResetClick = this.handleResetClick.bind(this)
+    }
 
     state = {
-        selected: null
+        from: null,
+        to: null
+    }
+
+    handleDayClick(e, day) {
+        const range = DateUtils.addDayToRange(day, this.state)
+        this.setState(range)
+    }
+
+    handleResetClick(e) {
+        e.preventDefault()
+        this.setState({
+            from: null,
+            to: null
+        })
     }
 
     render() {
@@ -19,11 +38,37 @@ class Container extends Component {
             label: article.title,
             value: article.id
         }))
+        const { from, to } = this.state
+
         return (
             <div>
-                <Select options = {options} value={this.state.selected} onChange = {this.handleChange} multi={true}/>
-                <ArticleList articles = {this.props.articles} />
-                <JqueryComponent items = {this.props.articles} ref={this.getJQ}/>
+                <div className="RangeExample">
+                    { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
+                    { from && !to && <p>Please select the <strong>last day</strong>.</p> }
+                    { from && to &&
+                    <p>
+                        You chose from { moment(from).format('L') } to { moment(to).format('L') }.
+                        { ' ' }<a href="#" onClick={ this.handleResetClick }>Reset</a>
+                    </p>
+                    }
+                    <DayPicker
+                        ref="daypicker"
+                        selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                        onDayClick={ this.handleDayClick }
+                    />
+                </div>
+                <Select
+                    options = {options}
+                    value={this.state.selected}
+                    onChange = {this.handleChange}
+                    multi={true}
+                />
+                <ArticleList
+                    articles = {this.props.articles}
+                />
+                <JqueryComponent
+                    items = {this.props.articles} ref={this.getJQ}
+                />
             </div>
         )
     }
